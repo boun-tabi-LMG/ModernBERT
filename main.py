58c256a62d93ca93086c569160bfa0da7a1003e0
Onur Gungor <onurgu@gmail.com>
2025-02-23 17:54:53 +0100

    logging trials 16

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ import src.text_data as text_data_module
 # from src.algorithms.rope_schedule import FlexBertRopeSchedule
 # from src.callbacks.dataloader_speed import DataloaderSpeedMonitor
 from src.callbacks.log_grad_norm import LogGradNorm
-# from src.callbacks.packing_efficiency import PackingEfficency
+from src.callbacks.packing_efficiency import PackingEfficency
 from src.callbacks.scheduled_gc import ScheduledGarbageCollector
 from src.scheduler import CosineInverseSqrtScheduler, OneMinusSqrtScheduler, WarmupStableDecayScheduler
 from src.sequence_packer import get_num_samples_in_packed_batch, split_packed_batch
