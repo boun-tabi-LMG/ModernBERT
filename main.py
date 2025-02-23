967192720915cd7aeb411ae500d71472b5566056
Onur Gungor <onurgu@gmail.com>
2025-02-23 14:04:12 +0100

    logging trials 4

--- main.py
+++ main.py
@@ -35,17 +35,17 @@ from omegaconf import DictConfig, OmegaConf
 from omegaconf import OmegaConf as om
 from torch.optim import AdamW
 
-import src.flex_bert as flex_bert_module
-import src.hf_bert as hf_bert_module
-import src.mosaic_bert as mosaic_bert_module
-import src.text_data as text_data_module
-from src.algorithms.rope_schedule import FlexBertRopeSchedule
-from src.callbacks.dataloader_speed import DataloaderSpeedMonitor
-from src.callbacks.log_grad_norm import LogGradNorm
-from src.callbacks.packing_efficiency import PackingEfficency
-from src.callbacks.scheduled_gc import ScheduledGarbageCollector
-from src.scheduler import CosineInverseSqrtScheduler, OneMinusSqrtScheduler, WarmupStableDecayScheduler
-from src.sequence_packer import get_num_samples_in_packed_batch, split_packed_batch
+# import src.flex_bert as flex_bert_module
+# import src.hf_bert as hf_bert_module
+# import src.mosaic_bert as mosaic_bert_module
+# import src.text_data as text_data_module
+# from src.algorithms.rope_schedule import FlexBertRopeSchedule
+# from src.callbacks.dataloader_speed import DataloaderSpeedMonitor
+# from src.callbacks.log_grad_norm import LogGradNorm
+# from src.callbacks.packing_efficiency import PackingEfficency
+# from src.callbacks.scheduled_gc import ScheduledGarbageCollector
+# from src.scheduler import CosineInverseSqrtScheduler, OneMinusSqrtScheduler, WarmupStableDecayScheduler
+# from src.sequence_packer import get_num_samples_in_packed_batch, split_packed_batch
 
 import logging
 
