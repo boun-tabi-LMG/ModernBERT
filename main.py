53edd77f1c217ef895c04ea2586e44ac9861043e
Onur Gungor <onurgu@gmail.com>
2025-02-23 17:52:33 +0100

    logging trials 14

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ import src.text_data as text_data_module
 # from src.callbacks.dataloader_speed import DataloaderSpeedMonitor
 # from src.callbacks.log_grad_norm import LogGradNorm
 # from src.callbacks.packing_efficiency import PackingEfficency
-# from src.callbacks.scheduled_gc import ScheduledGarbageCollector
+from src.callbacks.scheduled_gc import ScheduledGarbageCollector
 from src.scheduler import CosineInverseSqrtScheduler, OneMinusSqrtScheduler, WarmupStableDecayScheduler
 from src.sequence_packer import get_num_samples_in_packed_batch, split_packed_batch
 
